e4496b75201cd150989dae4b2d7bdfb093eba477
bryanserfozo <bryan.serfozo@revature.com>
2024-12-10 11:41:39 -0500

    Pushing rest of Spring demo

--- week-01/demos/DemoApplication/src/main/java/com/revature/controllers/CustomerController.java
+++ week-01/demos/DemoApplication/src/main/java/com/revature/controllers/CustomerController.java
@@ -1,4 +1,79 @@
 package com.revature.controllers;
 
+import com.revature.models.Customer;
+import com.revature.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("customers") // Requests will be routed in this application and we need to declare what endpoints we
+// want to handle so we use @RequestMapping to give a parent mapping for all endpoints below
+// This class will handle all requests going to http://localhost:8080/customers
 public class CustomerController {
+
+    /*
+    This class will be in charge of handling web traffic
+    This class will have different endpoint declared to handle requests and will format responses accordingly
+
+    @Controller vs @RestController
+        @RestController is a combination of @Controller and @ResponseBody
+        @ResponseBody declares that the response from a method will fill in the response for the HTTP Request
+    Wire in the service class
+     */
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    // Todo Get All Customer Methods
+    @GetMapping // This will be called when a GET request comes in
+    public List<Customer> getAllCustomersHandler(){
+        // I want this method to be executed when a GET Http Request comes in to http://localhost:8080/customers
+        return customerService.getAllCustomers();
+    }
+
+    // Todo Add a customer method
+    @PostMapping // POST request to http://localhost:8080/customers
+    public ResponseEntity<Customer> createCustomerHandler(@RequestBody Customer customer){
+        /*
+        We're expecting a customer to come from the request body we need to take it in as a method parameter
+        Once we have the customer object, we'll send it to the service layer to create the customer
+         */
+        Customer possibleCustomer = customerService.createNewCustomer(customer);
+
+        if (possibleCustomer == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else{
+            return new ResponseEntity<>(possibleCustomer, HttpStatus.CREATED);
+        }
+
+    }
+
+    // Todo Add a get single customer method
+    // This will likely also be a GET request but we need to be specific about the customer that we want
+    // I'm going to leverage their ID field to get started
+    @GetMapping("{customerId}") // This now handles GET requests to http://localhost:8080/customers/{customerId}
+    public ResponseEntity<Customer> getSpecificCustomerHandler(@PathVariable int customerId){
+        // Response Entities give you full control over how a Http Response is formatted so we can use it to define
+        // Specific status codes
+        // return customerService.getCustomerById(customerId).get();
+        Optional<Customer> possibleCustomer = customerService.getCustomerById(customerId);
+
+        if (possibleCustomer.isPresent()){
+            return new ResponseEntity<>(possibleCustomer.get(), HttpStatus.OK);
+        } else {
+            // We were unable to find the user with id "customerId"
+            // We should return a not found
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
 }
--- week-01/demos/DemoApplication/src/main/java/com/revature/services/CustomerService.java
+++ week-01/demos/DemoApplication/src/main/java/com/revature/services/CustomerService.java
@@ -1,8 +1,12 @@
 package com.revature.services;
 
 import com.revature.daos.CustomerDAO;
+import com.revature.models.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -27,8 +31,44 @@ public class CustomerService {
     }
 
     // Todo Get a Customer Method
+    public Optional<Customer> getCustomerById(int customerId){
+        // When in the service class our goal is to make sure that the data being entered and retrieved from the db
+        // is valid
+        // We need to leverage the dao layer to get info from the database
+
+        // What is an Optional?
+        // Wrapper class around objects that might be null to help with null checking
+        return customerDAO.findById(customerId);
+    }
 
     // TODO Get All customers Method
+    public List<Customer> getAllCustomers(){
+        return customerDAO.findAll();
+        // Why don't we use an optional here? The list will always exist whether or not there are any customers
+        // so no matter what this will not be null
+    }
 
     // Todo Add new customer method
+    public Customer createNewCustomer(Customer customer){
+        // Validation logic here
+        if (customer.getPurchaseAmount() <= 0){
+            return null;
+        }
+
+        return customerDAO.save(customer);
+    }
 }
+
+/*
+What is Dependency Injection? Dependency Injection is a design pattern where the dependencies of a class are determined
+in some external configuration location. This could another class like a Factory class, or in our case, the Spring
+Framework itself. Spring is responsible for creating, managing and wiring our classes together
+
+How do we do dependency injection in Spring?
+Two (technically 3 ways)
+Constructor Injection: This is injecting dependencies through your constructor, standard practice and will be used
+for 95% of your use cases
+Setter Injection: Using @Autowired over a setter to set after the class has been initialized. Good for circular
+dependencies. (Class A depends on Class B but Class B also depends on Class A)
+Field Injection: This is using @Autowired directly on the field
+ */
